27f384dafc002c8771584b08efa16bceb7bd533c
Tejas Bhartiya <32378536+TejasCreative@users.noreply.github.com>
2023-04-06 07:53:14 -0700

    1

---
+++ visual.h
@@ -0,0 +1,29 @@
+#ifndef VISUAL_H
+#define VISUAL_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ostream>
+
+#include "coord.h"
+#include "matrix.h"
+#include "node.h"
+
+
+class Visual{
+    public:
+        Visual(){
+            //std::cout << "Visual constructor" << std::endl;
+
+        }
+        void visualize()
+
+
+
+};
+
+
+
+
+#endif
